f4d42451d2bb170bef71804889e34be40d922732
NgeKaworu <NgeKaworu@163.com>
2019-11-16 22:25:25 +0800

    命名规范

--- native/request-core.js
+++ native/request-core.js
@@ -3,13 +3,13 @@ import request from 'umi-request';
 export default (
   url,
   {
-    bussStatusHandler = reps => reps,
+    extraHandler = reps => reps,
     successHandler = reps => reps,
     errorHandler = err => err,
     ...options
   }
 ) =>
   request(url, { getResponse: true, ...options })
-    .then(bussStatusHandler)
+    .then(extraHandler)
     .then(successHandler)
     .catch(errorHandler);
