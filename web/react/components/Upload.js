9ca50a80321b472ff9dae6300bfa17d28c34418a
宁泰霖 <NgeKaworu@163.com>
2019-11-22 18:00:47 +0800

    准备写 upload 组件

--- web/react/components/Upload.js
+++ web/react/components/Upload.js
@@ -1,57 +1,22 @@
-import React, { forwardRef, useState } from 'react';
-import { Upload, Icon } from 'antd';
+import React, { forwardRef } from 'react';
+import styled from 'styled-components';
+
+const Wrap = styled.div``;
 
 export default forwardRef(
-  (
-    {
-      value,
-      placeholder,
-      onChange,
-      fileHandler = () => {},
-      prefix,
-      ...restProps
-    },
-    ref,
-  ) => {
-    const [getImgUrl, setImgUrl] = useState(value);
-
-    const uploadButton = (
-      <div>
-        <Icon type='plus' />
-        <div>{placeholder}</div>
-      </div>
-    );
-
+  ({ value, onChange, children, ...restProps }, ref) => {
     return (
-      <Upload
-        value={value}
-        ref={ref}
-        customRequest={async ({ file, onProgress, onError, onSuccess }) => {
-          try {
-            onProgress(0);
-            const url = await fileHandler(file);
-            onProgress(100);
-            onSuccess({
-              url,
-            });
-            onChange(url);
-            setImgUrl(url);
-          } catch (e) {
-            onError(e);
-          }
-        }}
-        {...restProps}
-      >
-        {getImgUrl ? (
-          <img
-            src={`${prefix}${getImgUrl}`}
-            alt='avatar'
-            style={{ width: '100%' }}
-          />
-        ) : (
-          uploadButton
-        )}
-      </Upload>
+      <Wrap>
+        <input
+          type='file'
+          ref={ref}
+          onChange={(...arg) => {
+            console.log(arg);
+          }}
+          {...restProps}
+        />
+        {children}
+      </Wrap>
     );
   },
 );
---
+++ web/react/components/Upload2.js
@@ -0,0 +1,57 @@
+import React, { forwardRef, useState } from 'react';
+import { Upload, Icon } from 'antd';
+
+export default forwardRef(
+  (
+    {
+      value,
+      placeholder,
+      onChange,
+      fileHandler = () => {},
+      prefix,
+      ...restProps
+    },
+    ref,
+  ) => {
+    const [getImgUrl, setImgUrl] = useState(value);
+
+    const uploadButton = (
+      <div>
+        <Icon type='plus' />
+        <div>{placeholder}</div>
+      </div>
+    );
+
+    return (
+      <Upload
+        value={value}
+        ref={ref}
+        customRequest={async ({ file, onProgress, onError, onSuccess }) => {
+          try {
+            onProgress(0);
+            const url = await fileHandler(file);
+            onProgress(100);
+            onSuccess({
+              url,
+            });
+            onChange(url);
+            setImgUrl(url);
+          } catch (e) {
+            onError(e);
+          }
+        }}
+        {...restProps}
+      >
+        {getImgUrl ? (
+          <img
+            src={`${prefix}${getImgUrl}`}
+            alt='avatar'
+            style={{ width: '100%' }}
+          />
+        ) : (
+          uploadButton
+        )}
+      </Upload>
+    );
+  },
+);
