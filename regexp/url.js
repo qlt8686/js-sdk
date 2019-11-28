903e56e22593d3f023f1b5d298efce4228c95a05
NgeKaworu <NgeKaworu@163.com>
2019-11-28 14:38:36 +0800

    url 正则 所有A标签

--- regexp/url.js
+++ regexp/url.js
@@ -1,3 +1,36 @@
 export default new RegExp(
   'https?://[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]',
 );
+
+
+// 匹配所有链接
+// const firstSplit = new RegExp(
+//   '.*https?://[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|].*',
+//   'g',
+// );
+// const secondSplit = new RegExp(
+//   '(.*)(https?://[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|])(.*)',
+// );
+// const matchRroup = value.match(firstSplit);
+// // console.log('matchRroup', matchRroup);
+// return matchRroup?.map((i, idx) => {
+//   const splitUrl = i.match(secondSplit);
+//   const [pre, url, next] = [splitUrl[1], splitUrl[2], splitUrl[3]];
+//   return (
+//     <Fragment key={idx}>
+//       {pre}
+//       <a
+//         alt='外链'
+//         style={{
+//           color: linkColor,
+//         }}
+//         href={url}
+//         rel='noopener noreferrer'
+//         target='_blank'
+//       >
+//         {url}
+//       </a>
+//       {next}
+//     </Fragment>
+//   );
+// });
