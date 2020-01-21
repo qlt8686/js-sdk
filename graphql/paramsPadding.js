29f5e10c2d658097fa61a2a2b651d1fa5158238d
NgeKaworu <NgeKaworu@163.com>
2020-01-21 12:07:54 +0800

    参数填充

--- graphql/paramsPadding.js
+++ graphql/paramsPadding.js
@@ -1,8 +1,8 @@
-/**
- * 对象转String
- * @param {入参 | Obj} params
- * @return { 出参 | String}
- */
+// /**
+//  * 对象转String
+//  * @param {入参 | Obj} params
+//  * @return { 出参 | String}
+//  */
 export default function paramsPadding(params) {
   return Object.keys(params).reduce(
     (acc, cur) => `${acc} ${cur}: ${assginType(params[cur])}`,
@@ -13,10 +13,33 @@ export default function paramsPadding(params) {
 function assginType(value) {
   switch (typeof value) {
     case 'number':
+    case 'boolean':
       return value;
     case 'string':
       return `"${value}"`;
+    case 'object':
+      return Array.isArray(value) ? a2s(value) : o2s(value);
     default:
       throw new Error('no valid type', typeof value, value);
   }
+}
+
+function a2s(arr) {
+  return `[
+    ${arr.reduce(
+      (acc, cur) => `${acc}
+    ${assginType(cur)}`,
+      '',
+    )}
+  ]`;
+}
+
+function o2s(obj) {
+  return `{
+    ${Object.keys(obj).reduce(
+      (acc, cur) => `${acc}
+    ${cur}: ${assginType(obj[cur])}`,
+      '',
+    )}
+  }`;
 }
