93e3bd2b7382d3732f6114880435262d1a5b94c2
NgeKaworu <NgeKaworu@163.com>
2020-02-18 17:58:45 +0800

    使用类方式

--- graphql/Outputer.js
+++ graphql/Outputer.js
@@ -1,32 +1,34 @@
-function Outputer(...args) {
-  this.output = args;
-}
+class Outputer {
+  constructor(...args) {
+    this.output = args;
+  }
 
-Outputer.prototype.toString = function() {
-  return `{${this.output.reduce(
-    (acc, cur) =>
-      `${acc} ${
+  toString = () => {
+    return `{${this.output.reduce(
+      (acc, cur) =>
+        `${acc} ${
+          typeof cur === 'string'
+            ? cur
+            : Object.keys(cur).reduce((_, c) => `${c} ${cur[c]}`, '')
+        },`,
+      '',
+    )}}`;
+  };
+
+  just = (...args) => {
+    return new Outputer(...args);
+  };
+
+  filter = (...args) => {
+    return new Outputer(
+      ...this.output.filter(cur =>
         typeof cur === 'string'
-          ? cur
-          : Object.keys(cur).reduce((_, c) => `${c} ${cur[c]}`, '')
-      },`,
-    '',
-  )}}`;
-};
-
-Outputer.prototype.just = function(...args) {
-  return new Outputer(...args);
-};
-
-Outputer.prototype.filter = function(...args) {
-  return new Outputer(
-    ...this.output.filter(cur =>
-      typeof cur === 'string'
-        ? !args.includes(cur)
-        : args.includes(Object.keys(cur)[0]),
-    )
-  );
-};
+          ? !args.includes(cur)
+          : args.includes(Object.keys(cur)[0]),
+      ),
+    );
+  };
+}
 
 function outputerFactory(...args) {
   return new Outputer(...args);
