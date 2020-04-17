c132566da7203f63813e10853102353c032cd42d
NgeKaworu <NgeKaworu@163.com>
2020-04-17 16:38:00 +0800

    浏览器复制函数

---
+++ web/third-party/copy.js
@@ -0,0 +1,13 @@
+export default function copy(value, success = () => {}, fail = () => {}) {
+  const input = document.createElement('input');
+  document.body.appendChild(input);
+  input.setAttribute('value', value);
+  input.select();
+  const ok = document.execCommand('copy');
+  if (ok) {
+    success();
+  } else {
+    fail();
+  }
+  document.body.removeChild(input);
+}
